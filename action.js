683cbb539d723d00a5289fd31785f44361e1e455
HazelMary <64922924+HazelMary@users.noreply.github.com>
2021-05-17 20:26:44 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,11 @@
+function earth(){
+    document.body.style.background="#a2836e"
+}
+
+function fire(){
+    document.body.style.background="#ff7b25"
+}
+
+function water(){
+    document.body.style.background="#87bdd8"
+}
